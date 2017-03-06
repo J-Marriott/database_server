6ed0e9744720c15714d2726f5f9f505a8135eae1
Joe Marriott <marriott90@gmail.com>
2017-03-06 15:19:17 +0000

    cleanup using rubocop

--- lib/database_server.rb
+++ lib/database_server.rb
@@ -9,14 +9,14 @@ class DatabaseServer < Sinatra::Base
   end
 
   get '/set' do
-    params.each {|key, value| DatabaseServer.cache[key] = value}
-    "#{DatabaseServer.cache}"
+    params.each { |key, value| DatabaseServer.cache[key] = value }
+    DatabaseServer.cache.to_s
   end
 
   get '/get' do
-    "#{DatabaseServer.cache["#{params["key"]}"]}"
+    DatabaseServer.cache[params['key'].to_s].to_s
   end
 
   # start the server if ruby file executed directly
-  run! if app_file == $0
+  run! if app_file == $PROGRAM_NAME
 end
